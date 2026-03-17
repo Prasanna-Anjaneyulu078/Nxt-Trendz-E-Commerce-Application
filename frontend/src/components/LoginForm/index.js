41831caf2774a53f5b4db21bf2c27cf3f0aef5c4
Borigorla Venkata Prasanna Anjaneyulu <prasannaanjaneyulu078@gmail.com>
2026-03-17 14:09:27 +0530

    Update index.js

--- frontend/src/components/LoginForm/index.js
+++ frontend/src/components/LoginForm/index.js
@@ -1,7 +1,6 @@
 import {Component} from 'react'
 import Cookies from 'js-cookie'
-import {Redirect, Link} from 'react-router-dom'
-import {withRouter} from 'react-router-dom'
+import {Redirect, Link, withRouter} from 'react-router-dom'
 
 import './index.css'
 
